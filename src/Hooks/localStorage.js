a91db19b36c72188fa9986b0a33de173c974fc14
Shuvo Baroi <contact.shuvobaroi@gmail.com>
2024-01-10 15:16:13 +0600

    Tweak: Converted the home/front page responsive for all devices with off canvas popup for navigation

--- src/Hooks/localStorage.js
+++ src/Hooks/localStorage.js
@@ -6,3 +6,8 @@ export const getCartItems = () => {
     const items = JSON.parse(localStorage.getItem("cart"))
     return items;
 }
+
+export const getCartItem = () => {
+    const items = JSON.parse(localStorage.getItem("cart"))
+    return items;
+}
